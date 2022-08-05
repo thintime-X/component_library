7c0ead93f17933e773802a462c5d45113cc862cb
x <1743864770@qq.com>
2022-08-05 11:18:15 +0800

    删除flutter_smart_dialog包的引用

--- example/lib/my_app.dart
+++ example/lib/my_app.dart
@@ -1,4 +1,3 @@
-import 'package:component_library/component_library.dart';
 import 'package:flutter/material.dart';
 
 import 'my_home_page.dart';
@@ -21,35 +20,30 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      navigatorObservers: [
-        FlutterSmartDialog.observer,
-      ],
-      builder: FlutterSmartDialog.init(
-        builder: (context, child) {
-          return MediaQuery(
-            //设置文字大小不随系统设置改变
-            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-            child: Scaffold(
-              body: TextSelectionTheme(
-                data: TextSelectionThemeData(
-                  cursorColor: Theme.of(context).primaryColor,//输入框光标颜色
-                ),
-                child: GestureDetector(
-                  onTap: () {
-                    //点击空白收起键盘
-                    FocusScopeNode currentFocus = FocusScope.of(context);
-                    if (!currentFocus.hasPrimaryFocus &&
-                        currentFocus.focusedChild != null) {
-                      FocusManager.instance.primaryFocus?.unfocus();
-                    }
-                  },
-                  child: child,
-                ),
+      builder: (context, child) {
+        return MediaQuery(
+          //设置文字大小不随系统设置改变
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: Scaffold(
+            body: TextSelectionTheme(
+              data: TextSelectionThemeData(
+                cursorColor: Theme.of(context).primaryColor,//输入框光标颜色
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  //点击空白收起键盘
+                  FocusScopeNode currentFocus = FocusScope.of(context);
+                  if (!currentFocus.hasPrimaryFocus &&
+                      currentFocus.focusedChild != null) {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  }
+                },
+                child: child,
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
